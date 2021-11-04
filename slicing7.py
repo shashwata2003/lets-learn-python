cb8bf27b48dd290f03f5449ec5592781f1e11394
shashwata <shashwatasamanta5@gmail.com>
2021-11-04 11:40:29 +0530

    Added more practice problems

--- slicing7.py
+++ slicing7.py
@@ -1,1 +1,12 @@
 """Write a program to check if two strings are balanced. For example, strings s1 and s2 are balanced if all the characters in the s1 are present in s2. The character’s position doesn’t matter."""
+
+s1=input()
+s2=input()
+res='true'
+for i in s1:
+    if i in s2:
+       res='true'
+        
+    else:
+        res='false'
+print(res)
--- slicing8.py
+++ slicing8.py
@@ -1,7 +1,12 @@
-import string
-x=input()
+"""Given a string s1, write a program to return the sum and average of the digits that appear in the string, ignoring all other characters"""
+str=input()
+sum=0;
+cnt=0
+for i in str:
+    if i.isdigit():
+        sum=sum+int(i)
+        cnt+=1   
+aveg=sum/cnt;
 
-for a in string.punctuation:
-        newstr=x.replace(a,'#')
-    
-print(newstr)
+print(sum)
+print(aveg)
